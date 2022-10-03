f16242f4d2fdb5356c59bf7d56acdf20ecbe3fe5
John <John01166@ya.ru>
2022-10-03 15:13:25 +0300

    003 Random

--- Examples003_sum/Program.cs
+++ Examples003_sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numA = 422;
-int numB = 5;
+﻿int numA = new Random().Next(1,10);
+Console.WriteLine(numA);
+int numB = new Random().Next(1,10);
+Console.WriteLine(numB);
+
 int result = numA + numB;
 Console.WriteLine(result);
-fsdf
