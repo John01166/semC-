68b6f66067ec497269bce8aa7c1772f9dd73aff7
John <John01166@ya.ru>
2022-10-03 16:24:25 +0300

    006 base

---
+++ Examples006_base/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = new Random().Next(1,10);
+Console.WriteLine(a);
+int b = new Random().Next(1,10);
+Console.WriteLine(b);
+int c = new Random().Next(1,10);
+Console.WriteLine(c);
+int d = new Random().Next(1,10);
+Console.WriteLine(d);
+int e = new Random().Next(1,10);
+Console.WriteLine(e);
+int f = new Random().Next(1,10);
+Console.WriteLine(f);
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = c;
+if (f>max) max = f;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
