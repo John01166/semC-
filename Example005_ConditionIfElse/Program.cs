ce69c18e67cc65e09e2b4a73b0076a9b98fcf5a2
John <John01166@ya.ru>
2022-10-03 15:35:24 +0300

    005 ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Пожалуйста представьтесь (имя пользователя)");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет, тварь!");
+}
+else
+{
+    Console.WriteLine($"добрый вечер {username}");
+}
