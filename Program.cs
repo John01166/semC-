e1ad6d9458daa8225a47636c46fd3f9d7dbf4990
John01166 <114170510+John01166@users.noreply.github.com>
2022-10-04 14:40:42 +0300

    ДЗ. ЗАдача 1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите два целых числа");
+Console.WriteLine("Первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num2>max) max = num2;
+Console.Write("max = ");
+Console.WriteLine(max);
